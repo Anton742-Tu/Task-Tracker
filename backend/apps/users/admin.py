3ebfbea025b53208389fb7513738ea7fb0350aa8
Anton Tumashov <Tumashovster@Gmail.com>
2026-01-21 12:50:58 +0500

    Fix mypy: исправлены ошибки типизации в admin.py

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from typing import cast, List, Tuple
+from django.utils.translation import gettext_lazy as _
 
 from .models import User
 
@@ -8,12 +10,15 @@ from .models import User
 class CustomUserAdmin(UserAdmin):
     list_display = ("username", "email", "first_name", "last_name", "role", "is_staff")
     list_filter = ("role", "is_staff", "is_superuser")
-    fieldsets = UserAdmin.fieldsets + (
-        ("Дополнительная информация", {"fields": ("role", "bio", "avatar")}),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        ("Дополнительная информация", {"fields": ("role",)}),
-    )
+
+    # Приводим UserAdmin.fieldsets к списку
+    fieldsets = cast(List[Tuple], UserAdmin.fieldsets) + [
+        (_("Дополнительная информация"), {"fields": ("role", "bio", "avatar")}),
+    ]
+
+    add_fieldsets = cast(List[Tuple], UserAdmin.add_fieldsets) + [
+        (_("Дополнительная информация"), {"fields": ("role",)}),
+    ]
 
     def get_queryset(self, request):
         """Менеджеры видят всех пользователей, но не могут редактировать админов"""
